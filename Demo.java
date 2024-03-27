f054e12d535b6354fdc5696681cea06da13e7e17
shantanunimbalkar <shantanu.nimbalkar@embold.io>
2024-03-27 16:17:42 +0530

    add

--- Demo.java
+++ Demo.java
@@ -2,8 +2,9 @@ package NewDemo;
 
 public class Hello{
   public printSTMT(){
-    //System.out.println("Hello");
-	//System.out.println("Hello");
+    System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
