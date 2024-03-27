db022dd1fc19b207a2e7f07b0ac910cc8597debe
Team Embold <Team.Embold@godigit.com>
2024-03-27 13:16:22 +0530

    added [Embold Quality Gate:Passed]
    *Current Corona Version*

--- Demo.java
+++ Demo.java
@@ -1,9 +1,10 @@
-//package NewDemo;
+package NewDemo;
 
 public class Hello{
   public printSTMT(){
     //System.out.println("Hello");
 	//System.out.println("Hello");
+	logger.info("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
   }
