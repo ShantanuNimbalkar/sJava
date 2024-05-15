24319dd07c657b8a673b5b6a4d801afe3b9851ef
shantanunimbalkar <shantanu.nimbalkar@embold.io>
2024-05-15 18:10:25 +0530

    added

---
+++ Demo copy.java
@@ -0,0 +1,18 @@
+package NewDemo;
+
+public class Hello{
+  public printSTMT(){
+    System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	  System.out.println("Hello");
+	  System.out.println("H");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+  }
+}
