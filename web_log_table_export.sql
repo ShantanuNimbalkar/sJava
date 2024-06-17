ccfb055404382157d80f774fc617cd3120d1dd35
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-06-17 16:07:25 +0530

    Update web_log_table_export.sql

--- web_log_table_export.sql
+++ web_log_table_export.sql
@@ -51,3 +51,19 @@ USE `web_logs` ;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
+CREATE OR REPLACE PACKAGE BODY date_utilities
+IS
+
+-- Take single parameter, relying on current default NLS date format
+FUNCTION strip_time (p_date IN DATE) RETURN DATE
+IS
+BEGIN
+   RETURN TO_DATE(TO_CHAR(p_date));
+END strip_time;
+
+
+END date_utilities;
+/
