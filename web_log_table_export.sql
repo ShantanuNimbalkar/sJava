ba3dc701dec1444ed05f74ebce743d12c3d01d98
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-06-17 16:42:03 +0530

    Update web_log_table_export.sql

--- web_log_table_export.sql
+++ web_log_table_export.sql
@@ -66,4 +66,16 @@ END strip_time;
 
 
 END date_utilities;
-/
+
+
+BEGIN
+FOR c1 IN (SELECT * FROM companies) LOOP
+  FOR c2 IN (SELECT * FROM contacts) LOOP
+    FOR c3 IN (SELECT * FROM parties) LOOP
+      NULL;
+    END LOOP;
+  END LOOP;
+END LOOP;
+END;
+
+
