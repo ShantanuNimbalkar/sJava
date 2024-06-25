2e6bd3adfeece7b8f91c83842511a9ef70f7afaf
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-06-25 18:34:29 +0530

    Merge pull request #4 from ShantanuNimbalkar/feature-2
    ticket-1211: added sysout

--- Demo.java
+++ Demo.java
@@ -2,11 +2,13 @@ package NewDemo;
 
 public class Hello{
   public printSTMT(){
-    System.out.println("Hello");
-	System.out.println("Hello");
-	System.out.println("Hello");
-	  System.out.println("Hello");
-	  System.out.println("H");
+//    System.out.println("Hello");
+//	System.out.println("Hello");
+//	System.out.println("Hello");
+//	  System.out.println("Hello");
+//	  System.out.println("H");
+//	System.out.println("H");
+//	System.out.println("H");
 	logger.info("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
