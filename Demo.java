6ee6c9d946265a4217926f002da9b5888e278474
shantanunimbalkar <shantanu.nimbalkar@embold.io>
2024-06-25 12:48:19 +0530

    ticket-1211: added sysout [Embold Quality Gate:Passed]
    *Current Corona Version*
    Version: 1.9.26.0-SNAPSHOT
    CommitID: 85ad9865bbc4853f91eaebfd664d98fdef5b0a68
    Timestamp: 2024-06-24 10:11:00 +0530
    
    [Embold Quality Gate:Passed]
    
    *Current Corona Version*
    Version: 1.9.26.0-SNAPSHOT
    CommitID: 0894297d43b1d628cdc99845dec4de4424006174
    Timestamp: 2024-06-23 19:49:25 +0000

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ package NewDemo;
 
 public class Hello{
   public printSTMT(){
+<<<<<<< HEAD
     System.out.println("Hello");
 	System.out.println("Hello");
 	System.out.println("Hello");
@@ -9,6 +10,15 @@ public class Hello{
 	  System.out.println("H");
 	  System.out.println("Hello");
 	  System.out.println("H");
+=======
+//    System.out.println("Hello");
+//	System.out.println("Hello");
+//	System.out.println("Hello");
+//	  System.out.println("Hello");
+//	  System.out.println("H");
+//	System.out.println("H");
+//	System.out.println("H");
+>>>>>>> e2c411c (ticket-1211: added sysout)
 	logger.info("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
