2300fb7e9e92e76c6137e02e5b73c681bd776ba9
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-04-04 14:30:42 +0530

    added unused variable

--- Demo/Outer.java
+++ Demo/Outer.java
@@ -3,6 +3,7 @@ public class Outer {
   Inner ic = new Inner();//Causes generation of accessor class
   int jk = 211;
   int jkl = 211;
+  int unused = 401;
  }
  public class Inner {
   private Inner(){}
