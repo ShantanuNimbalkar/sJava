cedd10277e227e02d51f57bdc51794e4036f398c
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-06-19 18:26:18 +0530

    Update Demo copy.java

--- New Folder/Demo copy.java
+++ New Folder/Demo copy.java
@@ -10,6 +10,7 @@ public class Hello {
 	  System.out.println("Hello");
 	  System.out.println("H");
 	logger.info("Hello");
+		inte=23;
 	logger.info("Hello");
 	logger.info("Hello");
 	logger.info("Hello");
