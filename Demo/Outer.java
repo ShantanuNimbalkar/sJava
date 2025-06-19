f648c8f2ed4cbd1b48626c49d48fb725e10b714d
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-06-19 18:53:20 +0530

    Update Outer.java

--- Demo/Outer.java
+++ Demo/Outer.java
@@ -1,6 +1,8 @@
 public class Outer {
  void method(){
   Inner ic = new Inner();//Causes generation of accessor class
+  System.out.println("Hello");
+  int unusedVariable=100;
  }
  public class Inner {
   private Inner(){}
