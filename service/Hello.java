e7e63fb45dbf57b383875da769b319295687e2ea
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-07-21 14:31:46 +0530

    Create Hello.java

---
+++ service/Hello.java
@@ -0,0 +1,33 @@
+package NewDemo;
+
+public class Hello{
+  public printSTMT(){
+    System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	  //System.out.println("Hello");
+	  System.out.println("H");
+	   System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	  System.out.println("Hello");
+	  System.out.println("H");
+System.out.println("Hello");
+	  System.out.println("Hello");
+	  System.out.println("H");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	logger.info("Hello");
+	 System.out.println("H");
+	//  if(true){}
+	//  try{}catch(Exception e){}
+	  //new code added on 11-Jul-2024
+	  System.out.println("Hello");
+	  System.out.println("Hello");
+	  System.out.println("H");
+  }
+}
