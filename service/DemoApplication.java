d7d23e83b7c4113718babaaf6ecbfeb2e4a87a62
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-07-21 14:33:47 +0530

    Create DemoApplication.java

---
+++ service/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+
+//test
+	@GetMapping("/")
+	public String home() {
+		try{
+
+		}catch(Exception e){
+			logger.error("Error :",e);
+		}
+		System.out.print("The addition of ");
+			System.out.print("The addition of ");
+   
+		return "Spring is here!";
+
+	}
+
+	public static void main(String[] args) {
+		            System.out.println("Login failed.");
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
