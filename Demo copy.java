cce867a8fe46b8ec58923984359ad7232ac03e1c
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-07-21 13:26:05 +0530

    Update Demo copy.java

--- Demo copy.java
+++ Demo copy.java
@@ -2,7 +2,7 @@ package NewDemo;
 
 public class Hello{
   public printSTMT(){
-    System.out.println("Hello");
+   // System.out.println("Hello");
 	System.out.println("Hello");
 	System.out.println("Hello");
 	  System.out.println("Hello");
